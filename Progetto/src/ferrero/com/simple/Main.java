6a24b0e9e770c06758bc8eee2d80c0377bcdeff7
drugotosto <ferrero.maurizio84@gmail.com>
2018-03-13 18:47:58 +0100

    Sperimentato con la parola chiave 'static' e implementazione del SINGLETON PATTERN sotto forma della classe 'Zoo'

--- Progetto/src/ferrero/com/simple/Main.java
+++ Progetto/src/ferrero/com/simple/Main.java
@@ -1,19 +1,21 @@
 package ferrero.com.simple;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-class Main {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 	  
-	  System.out.print("Inserisci il nome del cane: ");
+    System.out.print("Inserisci il nome del cane: ");
     String nome = sc.nextLine();
     System.out.print("Inserisci l'età del cane: ");
     String eta = sc.nextLine();
     System.out.print("Inserisci la razza del cane: ");
     String razza = sc.nextLine();
     
-    // Cane dog1 = new Cane(nome,eta,razza);
+    Cane dog1 = new Cane(nome,eta,razza);
 
 		// System.out.println("La classe dell'oggetto 'dog' è: "+dog1.getClass());
 		// System.out.println("L'hashCode è: "+ dog1.hashCode());
@@ -26,6 +28,15 @@ class Main {
 
 		Lion leone1 = new Lion("Mufasa","35");
 		System.out.println("\n"+leone1.toString());
+
+		// Utilizzo della sola e unica istanza appartenete alla classe "CustomMath" = SINGLETON PATTERN 
+		Zoo zoo = Zoo.setInstance(new ArrayList<>(Arrays.asList(dog1,leone1)));
+		System.out.println("\nL'istanza ritorna è: "+ zoo);
+		System.out.println("L'istanza della classe appena creata è: "+ Zoo.getInstance());
+		System.out.println("Lo zoo è formato da: "+ zoo.getAnimali());
+		// Aggiunta di un cane allo Zoo
+		zoo.addAnimale(dog2);
+		System.out.println("Lo zoo è formato da: "+ zoo.getAnimali());
 	}
-	
+
 }
---
+++ Progetto/src/ferrero/com/simple/Zoo.java
@@ -0,0 +1,34 @@
+package ferrero.com.simple;
+
+import java.util.List;
+/*
+	Classe che implementa il SINGLETON PATTERN.
+	Tramite l'utilizzo combinato delle parole chiave "static", "private" viene definita una classe che permette la creazione di una sola istanza/oggetto "Zoo" per l'intera applicazione e che attraverso gli altri metodi non statici permette di implementare la logica della sudetta classe.
+*/
+public class Zoo {
+	
+	private static Zoo instance = new Zoo();
+	private List<Animale> animali;
+
+	private Zoo() {
+	}
+
+	// Una volta inizializzato l'unica istanza "Zoo" con la lista passata l'oggetto appena creato viene restituito
+	public static Zoo setInstance(List<Animale> animali) {
+		instance.animali = animali;
+		return instance;
+	}
+
+	public static Zoo getInstance() {
+		return instance;
+	}
+
+	public List<Animale> getAnimali() {
+		return instance.animali;
+	}
+
+	public void addAnimale(Animale animale) {
+		instance.animali.add(animale);
+	}
+
+}
